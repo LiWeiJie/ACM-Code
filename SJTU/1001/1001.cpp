b4eb8f794256005fb20bfbbb94e27f34c03196db
LiWeiJie <apprentice.amber@gmail.com>
2012-08-26 10:20:45 +0800

    commit University of Chicago Invitational 2012

---
+++ SJTU/1001/1001.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iomanip>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    int t,chair,n;
+    scanf("%d %d %d",&t,&chair,&n);
+    int ans = 0,tmp;
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&tmp);
+        if (tmp<=(t+chair)) ans ++ ;
+    }
+    printf("%d\n",ans);
+    return 0;
+}
---
+++ University of Chicago Invitational 2012/1003/1003.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iomanip>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <list>
+//#include <iostream>
+#define N 801
+
+using namespace std;
+
+long long lcm(long long m, long long n)
+{
+	long long i,t;
+	if(m<n)
+	 {t=m;m=n;n=t;}
+	for(i=m;i>=1;i++)
+	 //if(i/m*m==m&&i/n*n==n) 这样是不对的i/m是整除的，比如3/4=1
+	 if(i%m==0&&i%n==0)//判断整除是这么判断的
+		 break;
+	return i;
+}
+
+
+
+int main()
+{
+    int n,k;
+    int *groupA,*groupB,*change;
+    groupA = new int[N];
+    groupB = new int[N];
+
+    while (scanf("%d %d",&n,&k)!=EOF)
+//    while (cin >> n >> k)
+    {
+//        n =800;
+//        k = 11;
+        bool mk[N] ;
+        int num=0;
+        if (n==0&&k==0) break;
+        bool f =true;
+        memset(mk,false,sizeof(mk));
+        long long ans = 0;
+        long long fans = 1;
+
+//        change = new int[n+1];
+        for (int i=1;i<=n;i++)
+                groupA[i] = i ;
+        int base = n/k;
+        while (1)
+        {
+            f = false;
+            ans ++;
+            int tmp = n % k ;
+            int mul = 1;
+            int pos;
+            for (int i = 1;i<=n;i++)
+            {
+                if (mul == k+1) mul = 1;
+//                if (tmp==0)
+//                {
+//                    pos =mul*base-(i)/k;
+//                }
+//                else
+//                {
+                    if (mul>tmp) pos = tmp*(base+1)+(mul-tmp)*base-(i-1)/k;
+                    else pos = mul*(base+1)-(i-1)/k;
+
+
+//                }
+
+                groupB[pos] = groupA[i] ;
+
+                if (!mk[pos]&&groupB[pos] == pos)
+                {
+                    num++;
+                    mk[pos] = true;
+                    fans =lcm(fans,ans);
+                }
+
+
+                mul++;
+            }
+//            for (int i=1;i<=n;i++)
+//            {
+//                if (groupB[i]!=i)
+//                {
+//                    f = true;
+//                    break;
+//                }
+//            }
+            if (num==n) break;
+            change = groupA;
+            groupA = groupB;
+            groupB = change;
+        }
+        printf("%lld\n",ans);
+//        cout << ans << endl;
+
+    }
+    return 0;
+}
