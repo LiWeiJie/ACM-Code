c8a76ac8cbc427d4b758bd08b72509fe44ff5acc
LiWeiJie <apprentice.amber@gmail.com>
2012-08-26 15:23:21 +0800

    finish University of Chicago Invitational 2012:1003

---
+++ University_of_Chicago_Invitational_2012/1002/1002.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+//#include <algorithm>
+//#include <iomanip>
+#include <cstring>
+//#include <vector>
+//#include <queue>
+//#include <list>
+
+using namespace std;
+
+int main()
+{
+    int l,w;
+    while (scanf("%d %d",&l,&w)!=EOF)
+    {
+        int ans = 0,tmp;
+
+            int ori[1000][1000];
+        //    int group[1000][1000];
+            int fin[1000][1000];
+        //    memset(group,0,sizeof(group));
+            memset(fin,0,sizeof(fin));
+            for (int i=0;i<l;i++)
+            {
+                for (int j=0;j<w;j++)
+                {
+                    scanf("%d",&ori[i][j]);
+                }
+            }
+            int a,b;
+            scanf("%d %d",&a,&b);
+            int sum = 0;
+            for (int i=0;i<l;i++)
+            {
+                for (int j=0;j<b;j++)
+                    fin[i][0] +=ori[i][j];
+                sum += fin[i][0];
+                if (i>=a-1)
+                {
+                    if (ans < sum) ans =sum;
+                    sum -= fin[i-a+1][0];
+                }
+            }
+
+            for (int i=1;i<w-b+1;i++)
+            {
+                sum = 0;
+                for (int j=0;j<l;j++)
+                {
+                    fin[j][i] = fin[j][i-1] - ori[j][i-1] + ori[j][i+b-1];
+                    sum += fin[j][i];
+                    if (j>=a-1)
+                    {
+                        if (ans < sum) ans =sum;
+                        sum -= fin[j-a+1][i];
+                    }
+                }
+            }
+
+
+        //    for (int i=0;i<a;i++)
+        //            group[0][0] += fin[i][0];
+        //    ans = group[0][0];
+        //    for (int i=1;i<l-a+1;i++)
+        //    {
+        //        group[i][0] = group[i-1][0] - fin[i-1][0] + fin [i+a-1][0];
+        //        if (ans < group[i][0]) ans = group[i][0];
+        //    }
+        ////
+        //    for (int i=0;i<w-b+1;i++)
+        //    {
+        //        for (int j=0;j<b;j++)
+        //            group[0][i]+=fin[j][i];
+        //        if (ans < group[0][i]) ans = group[0][i];
+        //    }
+        //
+        //
+        //    for (int i=1;i<l-a+1;i++)
+        //    {
+        //        for (int j=0;j<w-b+1;j++)
+        //        {
+        //            group[i][j] = group[i-1][j] - fin[i-1][j] + fin[i+a-1][j];
+        //            if (ans < group[i][j]) ans = group[i][j];
+        //        }
+        //    }
+//            for (int i=0;i<l-a+1;i++)
+//            {
+//                for (int j=0;j<w-b+1;j++)
+//                {
+//                    tmp = 0;
+//                    for (int k=i;k<i+a;k++)
+//                    tmp+= fin[k][j];
+//                    if (tmp>ans) ans =tmp;
+//                }
+//            }
+
+
+            printf("%d\n",ans);
+    }
+
+
+    return 0;
+}
---
+++ University_of_Chicago_Invitational_2012/1003/1003.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iomanip>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <list>
+//#include <iostream>
+#define N 801
+#define ll long long
+using namespace std;
+
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+
+int main()
+{
+    int n,k;
+    int *groupA;
+    groupA = new int[N];
+
+    while (scanf("%d %d",&n,&k)!=EOF)
+//    while (cin >> n >> k)
+    {
+//        n =800;
+//        k = 11;
+        bool mk[N] ;
+        int num=0;
+        if (n==0&&k==0) break;
+        memset(mk,false,sizeof(mk));
+
+
+
+//        change = new int[n+1];
+        int base = n/k;
+        int tmp = n % k ;
+        int mul = 1;
+        int pos;
+        long long ans = 1;
+        long long fans = 1;
+        for (int i = 1;i<=n;i++)
+        {
+            if (mul == k+1) mul = 1;
+
+            if (mul>tmp) pos = tmp*(base+1)+(mul-tmp)*base-(i-1)/k;
+            else pos = mul*(base+1)-(i-1)/k;
+
+
+//
+
+            groupA[i] = pos ;
+
+//            if (!mk[pos]&&groupA[pos] == pos)
+//            {
+//                num++;
+//                mk[pos] = true;
+//                fans =fans/gcd(fans,ans)*ans;
+//            }
+            mul++;
+        }
+
+
+
+
+
+        for (int i = 1;i<=n;i++)
+        {
+            tmp = i;
+            ans=0;
+            while (!mk[tmp])
+            {
+                ans++;
+                mk[tmp] = true;
+                tmp = groupA[tmp];
+            }
+            if (ans != 0 )
+            fans  = fans/gcd(fans,ans)*ans;
+        }
+//            for (int i=1;i<=n;i++)
+//            {
+//                if (groupB[i]!=i)
+//                {
+//                    f = true;
+//                    break;
+//                }
+//            }
+
+        printf("%I64d\n",fans);
+//        cout << ans << endl;
+
+    }
+    return 0;
+}
