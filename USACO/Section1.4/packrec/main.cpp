7d927e1a9f99de5fa9712388032d560dd2be914f
LiWeiJie <apprentice.amber@gmail.com>
2012-09-09 22:53:14 +0800

    usaco1.4 -1

--- USACO/Section1.4/packrec/main.cpp
+++ USACO/Section1.4/packrec/main.cpp
@@ -1,3 +1,10 @@
+/*
+    ID:apprent2
+    PROG:packrec
+    LANG:C++
+*/
+
+
 #include <stdio.h>
 #include <algorithm>
 #include <iomanip>
@@ -43,8 +50,7 @@ void ty2(NT a,NT b,NT c,NT d)
     NT nt;
     nt.p = b.p+c.p+d.p;
     nt.p = max(nt.p,a.q);
-    nt.q = b.q ;
-    nt.q = max(nt.q,c.q);
+    nt.q = max(b.q,c.q) ;
     nt.q = max(nt.q,d.q);
     nt.q += a.p;
     if (nt.q<nt.p) swap(nt.p,nt.q);
@@ -75,37 +81,40 @@ void ty4(NT a,NT b,NT c,NT d)
     ans.push(nt);
 }
 
+
 void ty5(NT a,NT b,NT c,NT d)
 {
     NT nt;
-    nt.p = max(a.p,b.p)+d.p;
-    int cd = c.p+d.q,ab = a.q+b.q;
-    if (ab>d.q)
+    int l, r;
+    r = b.q+d.q;
+    l = a.q+c.q;
+    nt.q = max(r,l);
+    r = max(b.p,d.p);
+    l = max(a.p,c.p);
+    nt.p = l+r;
+    if ( c.p<a.p && b.p<d.p)
     {
-        nt.p = max(nt.p,c.q+a.p);
+        int tmp;
+        if (a.q<b.q)
+        {
+            tmp = a.p-c.p;
+            tmp = min(tmp,d.p-b.p);
+            nt.p =nt.p-2*tmp;
+        }
     }
-    else
-    {
-        nt.p = max(nt.p,c.q);
-    }
-    nt.q = max(a.q+b.q,c.p+d.q);
+
     if (nt.q<nt.p) swap(nt.p,nt.q);
     ans.push(nt);
+
 }
 
-void ty6(NT a,NT b,NT c,NT d)
-{
-    NT nt;
-    nt.p = d.q;
-    int cd = c.p+d.q,ab = a.p+max(b.q,c.p);
-    nt.p = max(nt.p,ab);
-    nt.q = max(c.q+b.p,a.q);
-    if (nt.q<nt.p) swap(nt.p,nt.q);
-    ans.push(nt);
-}
+
+
 
 int main()
 {
+//    freopen("packrec.in","r",stdin);
+//    freopen("packrec.out","w",stdout);
     NT g[4];
     bool f[5];
     memset(f,0,sizeof(f));
@@ -122,61 +131,93 @@ int main()
         while(f[i]) i++;
         if (i>3) break;
         f[i] = true;
-		for (int j=0;j<4;j++)
-		{
-		    while(f[j]) j++;
-            if (j>3) break;
-            f[j] = true;
-		    for (int x=0;x<4;x++)
-		    {
-		        while(f[x]) x++;
-                if (x>3) break;
-                f[x] = true;
-		        for (int y=0;y<4;y++)
-		        {
-		            while(f[y]) y++;
-                    if (y>3) break;
+        for (int ap=0;ap<2;ap++)
+        {
+            swap(g[i].p,g[i].q);
 
-
-                    ty1(g[i],g[j],g[x],g[y]);
-                    NT no = ans.top();
-                    ty2(g[i],g[j],g[x],g[y]);
-                    no = ans.top();
-                    ty3(g[i],g[j],g[x],g[y]);
-                    no = ans.top();
-                    ty4(g[i],g[j],g[x],g[y]);
-                    no = ans.top();
-//                    ty5(g[i],g[j],g[x],g[y]);
-                    ty6(g[i],g[j],g[x],g[y]);
-                    if (ans.size())
+            for (int j=0;j<4;j++)
+            {
+                while(f[j]) j++;
+                if (j>3) break;
+                f[j] = true;
+                for (int bp=0;bp<2;bp++)
+                {
+                    swap(g[j].p,g[j].q);
+                    for (int x=0;x<4;x++)
                     {
-
-                        if (no.p==5 && no.q == 9)
+                        while(f[x]) x++;
+                        if (x>3) break;
+                        f[x] = true;
+                        for (int cp=0;cp<2;cp++)
                         {
-                            no.p = 0;
-                            no.q = 0;
+                            swap(g[x].p,g[x].q);
+                            for (int y=0;y<4;y++)
+                            {
+                                while(f[y]) y++;
+                                if (y>3) break;
+                                for (int dp=0;dp<2;dp++)
+                                {
+                                    swap(g[y].p,g[y].q);
+                                    ty1(g[i],g[j],g[x],g[y]);
+                //                    NT no = ans.top();
+                                    ty2(g[i],g[j],g[x],g[y]);
+                //                    no = ans.top();
+                                    ty3(g[i],g[j],g[x],g[y]);
+                //                    no = ans.top();
+                                    ty4(g[i],g[j],g[x],g[y]);
+
+                //                    ty5(g[i],g[j],g[x],g[y]);
+                                    ty5(g[i],g[j],g[x],g[y]);
+                //                    if (ans.size())
+                //                    {
+                //
+                //                        if (no.p==5 && no.q == 9)
+                //                        {
+                //                            no.p = 0;
+                //                            no.q = 0;
+                //                        }
+                //                    }
+                                }
+
+
+
+                            }
                         }
+                        f[x] = false;
                     }
 
+                }
+                f[j] = false;
+            }
+        }
 
-		        }
-		        f[x] = false;
-		    }
-		    f[j] = false;
-		}
 		f[i] = false;
     }
     int maxn;
     NT tmp = ans.top();
     maxn = tmp.p*tmp.q;
     printf("%d\n",maxn);
-    while(tmp.p*tmp.q == maxn)
+    printf("%d %d\n",tmp.p,tmp.q);
+    NT temp = ans.top();
+//    while(ans.size())
+//    {
+//        NT temp = ans.top();
+//        printf("%d %d\n",temp.p,temp.q);
+//        ans.pop();
+//    }
+    while(temp.p*temp.q == maxn)
     {
-        printf("%d %d\n",tmp.p,tmp.q);
+
+        if (temp.p!=tmp.p||temp.q!=tmp.q)
+        {
+            tmp = ans.top();
+            printf("%d %d\n",tmp.p,tmp.q);
+        }
+
         ans.pop();
         if (ans.size()>0)
         {
-            tmp = ans.top();
+            temp = ans.top();
         }
         else
             break;
