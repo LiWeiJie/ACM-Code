038ef856916b7862fb5ac86e12020d4874cb4094
LiWeiJie <apprentice.amber@gmail.com>
2012-12-10 17:41:40 +0800

    zoj 1203 Swordfish
    修改一下

--- ZOJ/zoj 1203 Swordfish.cpp
+++ ZOJ/zoj 1203 Swordfish.cpp
@@ -1,4 +1,4 @@
-//zoj 1203 Swordfish
+﻿//zoj 1203 Swordfish
 /*
 题意要求令总的代价最小，很明显是最小生成树。
 在这里，很明显用Prim算法要比用Kruskal算法更方便
@@ -29,50 +29,62 @@ double distzero[MAXN];
 Point cites[MAXN];
 //记录城市是否在最小生成树中
 bool used[MAXN];
+int n;
+int ct = 0;
+
+
+void init()
+{
+    //计算好各city之间的距离
+    for (int i=0;i<n;i++){
+        scanf("%lf %lf",&cites[i].x,&cites[i].y);
+        for (int j=0 ;j<i;j++){
+            dists[i][j] = computerDist(cites[i],cites[j]);
+            dists[j][i] = dists[i][j];
+        }
+        used[i] = 0;
+        distzero[i] = MAX_DOUBLE;
+    }
+}
+void prim()
+{
+    distzero[0] = true;
+    used[0] = true;
+    int now = 0;
+    double sum = 0;
+    for (int i=0;i<n-1;i++){
+        double min = MAX_DOUBLE;
+        int next = 0;
+        for (int j=0;j<n;j++){
+            if (!used[j]){
+                if(distzero[j]>dists[now][j]){
+                    distzero[j] = dists[now][j];
+                }
+                if(min>distzero[j]){
+                    min = distzero[j];
+                    next = j;
+                }
+            }
+        }
+        sum += min;
+        now = next;
+        used[now] = true;
+    }
+    printf("Case #%d:\n", ct);
+    printf("The minimal distance is: ");
+    printf("%.2lf\n", sum);
+}
+
 int main()
 {
-	int n;
-	int ct = 0;
+
 	while(scanf("%d",&n)&&n!=0){
 		ct++;
 		//格式控制
 		if (ct!=1) printf("\n");
-		printf("Case #%d:\n", ct);
-		//计算好各city之间的距离
-		for (int i=0;i<n;i++){
-			scanf("%lf %lf",&cites[i].x,&cites[i].y);
-			for (int j=0 ;j<i;j++){
-				dists[i][j] = computerDist(cites[i],cites[j]);
-				dists[j][i] = dists[i][j];
-			}
-			used[i] = 0;
-			distzero[i] = MAX_DOUBLE;
-		}
-		distzero[0] = true;
-		used[0] = true;
-		int now = 0;
-		double sum = 0;
-		//prim算法求最小生成树
-		for (int i=0;i<n-1;i++){
-			double min = MAX_DOUBLE;
-			int next = 0;
-			for (int j=0;j<n;j++){
-				if (!used[j]){
-					if(distzero[j]>dists[now][j]){
-						distzero[j] = dists[now][j];
-					}
-					if(min>distzero[j]){
-						min = distzero[j];
-						next = j;
-					}
-				}
-			}
-			sum += min;
-			now = next;
-			used[now] = true;
-		}
-        printf("The minimal distance is: ");
-		printf("%.2lf\n", sum);
+		init();
+        prim();
 	}
     return 0;
 }
+
